dad4372e4f9a73668d4da06d5ae9ea2350aeafd1
LadyMadyQilin <d.franchet@gmail.com>
2023-11-07 15:22:44 +0100

    models Service ok

--- BACK/app/models/Service.js
+++ BACK/app/models/Service.js
@@ -2,22 +2,36 @@ import { DataTypes } from "sequelize";
 import sequelize from "../sequelize.js";
 
 const Service = sequelize.define("Service", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
   name_serv: {
-    type: DataTypes.STRING,
-    allowNull: true,
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  cover_serv: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+
   },
   description_serv: {
-    type: DataTypes.STRING,
-    allowNull: true,
+    type: DataTypes.STRING(1000),
+    allowNull: false,
   },
-  institutionId: {
-    type: DataTypes.INTEGER,
+  hour_start: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  hour_end: {
+    type: DataTypes.STRING,
     allowNull: false,
   },
   institution_id: {
     type: DataTypes.INTEGER,
-    allowNull: false,
-  },
+    allowNull: true,
+  }
 });
 
 export default Service;
