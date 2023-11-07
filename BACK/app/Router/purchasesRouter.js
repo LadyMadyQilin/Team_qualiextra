1d96316ed35ed99e7a844bc31dd7b624b90f1118
LadyMadyQilin <d.franchet@gmail.com>
2023-11-07 15:25:56 +0100

    router purchases ok

--- BACK/app/Router/purchasesRouter.js
+++ BACK/app/Router/purchasesRouter.js
@@ -7,15 +7,21 @@ import roleGuard from '../middlewares/roleGuard.js';
 const purchasesRouter = express.Router();
 
 //récupérer toutes les commandes
-purchasesRouter.get('/purchases', jwtGuard, roleGuard({ role: ['admin', 'provider'] }), userController.getAllPurchases)
-//récupérer une commande selon son id
-purchasesRouter.get('/purchases/:id', jwtGuard, roleGuard({ role: ['provider'] }), userController.getOnePurchase)
+purchasesRouter.get('/purchases', jwtGuard, roleGuard({ roles: ['admin', 'provider'] }), userController.getAllPurchases)
 
-//creation d'une commande
-purchasesRouter.post('/purchases', jwtGuard, roleGuard({ role: ['user'] }), userController.addPurchase)
+//récupérer une commande selon son id
+purchasesRouter.get('/purchases/:id', jwtGuard, roleGuard({ roles: ['provider'] }), userController.getOnePurchase)
+
+//créer d'une commande
+purchasesRouter.post('/purchases', jwtGuard, roleGuard({ roles: ['user'] }), userController.addPurchase)
+
+//ajouter package a purchase
+purchasesRouter.post('/purchase/package', jwtGuard, roleGuard({ roles: ['user'] }), userController.addPackageToPurchase)
+
 //modifier une commande
-purchasesRouter.put('/purchases/:id', jwtGuard, roleGuard({ role: ['user', 'provider'] }), userController.updatePurchase)
+purchasesRouter.put('/purchases/:id', jwtGuard, roleGuard({ roles: ['user', 'provider'] }), userController.updatePurchase)
+
 //supprimer une commande
-purchasesRouter.delete('/purchases/:id', jwtGuard, roleGuard({ role: ['user', 'provider'] }), userController.deletePurchase);
+purchasesRouter.delete('/purchases/:id', jwtGuard, roleGuard({ roles: ['user', 'provider'] }), userController.deletePurchase);
 
 export default purchasesRouter;
