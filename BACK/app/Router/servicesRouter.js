73be4fba545c5f70400e326122879c28d4ca6c48
LadyMadyQilin <d.franchet@gmail.com>
2023-11-07 15:27:10 +0100

    router services ok

--- BACK/app/Router/servicesRouter.js
+++ BACK/app/Router/servicesRouter.js
@@ -2,24 +2,24 @@ import express from 'express';
 
 import { jwtGuard } from '../middlewares/jwtGuard.js';
 import roleGuard from '../middlewares/roleGuard.js';
-import serviceProviderController from '../controllers/ProviderController/serviceProviderController.js';
+import serviceProviderController from '../controllers/providerController/serviceProviderController.js';
 
 
 const servicesRouter = express.Router();
+
 //récupérer tous les services
-servicesRouter.get('/services', jwtGuard, roleGuard({ role: ['admin', 'provider'] }), serviceProviderController.getAllServices);
+servicesRouter.get('/services', jwtGuard, roleGuard({ roles: ['admin', 'provider'] }), serviceProviderController.getAllServices);
 
-
-servicesRouter.get('/services/:id', jwtGuard, roleGuard({ role: ['admin', 'provider'] }), serviceProviderController.getOneService);
-
+// récupérer un service
+servicesRouter.get('/services/:id', jwtGuard, roleGuard({ roles: ['admin', 'provider'] }), serviceProviderController.getOneService);
 
 // créer un service
-servicesRouter.post('/services', jwtGuard, roleGuard({ role: ['provider'] }), serviceProviderController.addService);
+servicesRouter.post('/services', jwtGuard, roleGuard({ roles: ['provider'] }), serviceProviderController.addService);
 
 // 'modifier un service'
-servicesRouter.put('/services/:id', jwtGuard, roleGuard({ role: ['provider'] }), serviceProviderController.updateService);
+servicesRouter.put('/services/:id', jwtGuard, roleGuard({ roles: ['provider'] }), serviceProviderController.updateService);
 
 // 'supprimer un service'
-servicesRouter.delete('/services/:id', jwtGuard, roleGuard({ role: ['admin', 'provider'] }), serviceProviderController.deleteService);
+servicesRouter.delete('/services/:id', jwtGuard, roleGuard({ roles: ['admin', 'provider'] }), serviceProviderController.deleteService);
 
 export default servicesRouter;
