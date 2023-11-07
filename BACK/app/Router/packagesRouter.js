0d429e9e871a5fdf27050a17dcf5f2605843fc67
LadyMadyQilin <d.franchet@gmail.com>
2023-11-07 15:25:35 +0100

    router packages ok

--- BACK/app/Router/packagesRouter.js
+++ BACK/app/Router/packagesRouter.js
@@ -2,32 +2,39 @@ import express from 'express';
 
 import { jwtGuard } from '../middlewares/jwtGuard.js';
 import roleGuard from '../middlewares/roleGuard.js';
-import packageProviderController from '../controllers/ProviderController/packageProviderController.js';
+import packageProviderController from '../controllers/providerController/packageProviderController.js';
+import { availableController } from '../controllers/availableController.js';
 
 
 const packagesRouter = express.Router();
 
 //récupérer les meilleurs packages pour affichage page Home
-packagesRouter.get('/packages/:id/favorites', (req, res) => {
-    res.json('récupérer un forfait pour la homePage');
-});
+packagesRouter.get('/packages/:id/favorites', packageProviderController.bestPackageHome);
 
 //récupérer tous les packages
-packagesRouter.get('/packages', jwtGuard, roleGuard({ role: ['admin', 'provider'] }), packageProviderController.getAllPackages);
+packagesRouter.get('/packages', packageProviderController.getAllPackages);
 
-//récupérer un package selon son id 
-packagesRouter.get('/packages/:id', jwtGuard, roleGuard({ role: ['admin', 'provider'] }), packageProviderController.getOnePackage);
+//récupérer un package selon son id   
+packagesRouter.get('/package/:id', jwtGuard, roleGuard({ roles: ['admin', 'provider', 'user'] }), packageProviderController.getOnePackage);
 
 //récupérer l'identifant d'un package pour avoir avoir son prix
-packagesRouter.get('/packages/:id/prices', jwtGuard, roleGuard({ role: ['provider'] }), packageProviderController.getOnePrice);
+packagesRouter.get('/packages/:id/prices', jwtGuard, roleGuard({ roles: ['provider'] }), packageProviderController.getOnePrice);
 
 //création packages
-packagesRouter.post('/packages', jwtGuard, roleGuard({ role: ['admin', 'provider'] }), packageProviderController.addPackage);
+packagesRouter.post('/packages', jwtGuard, roleGuard({ roles: ['admin', 'provider'] }), packageProviderController.addPackage);
+
+//ajout de service dans package
+// jwtGuard, roleGuard({ roles: ['admin', 'provider'] }),
+packagesRouter.post('/package/service', packageProviderController.addServiceToPackage)
+
+//gestion de la quantité du stock et disponibilité
+packagesRouter.put('/purchases/packages/:id', availableController.availabilityManagement);
 
 //modification du forfait 
-packagesRouter.put('/packages/:id', jwtGuard, roleGuard({ role: ['admin', 'provider'] }), packageProviderController.updatePackage);
+packagesRouter.put('/packages/:id', jwtGuard, roleGuard({ roles: ['admin', 'provider'] }), packageProviderController.updatePackage);
 
 //supprimer forfait 
-packagesRouter.delete('/packages/:id', jwtGuard, roleGuard({ role: ['admin', 'provider'] }), packageProviderController.deletePackage);
+packagesRouter.delete('/packages/:id', jwtGuard, roleGuard({ roles: ['admin', 'provider'] }), packageProviderController.deletePackage);
+
 
 export default packagesRouter;
